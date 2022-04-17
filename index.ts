09657e0a69f50f8842fb4f56b531a011fe4e1e9b
Kim <DAVID.KIM@VIZIENTINC.COM>
2022-04-17 17:20:40 -0500

    .

--- index.ts
+++ index.ts
@@ -4,6 +4,6 @@ const config = new pulumi.Config();
 const stack = pulumi.getStack();
 const org = config.require("org");
 
-const stackRef = new pulumi.StackReference(`${org}/ VizientBigDataIntakePortal/${stack}`)
+const stackRef = new pulumi.StackReference(`${org}/VizientBigDataIntakePortal/${stack}`)
 
 export const shopUrl = stackRef.getOutput("url");
